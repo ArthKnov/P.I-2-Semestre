19d2e490ffffc93dffdc7cfdc227576a7c9ac04a
Arthur Lima <arthurlima121213@gmail.com>
2024-11-21 00:24:37 -0300

    Tentando fazer deploy

--- app.js
+++ app.js
@@ -949,7 +949,8 @@ if (userExists) {
     })(req, res, next);
 });
     // Inicializa o servidor
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
--- config/database.js
+++ config/database.js
@@ -1,16 +1,22 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME || "projeto_PI", 
-                                process.env.DB_USER || "root", 
-                                process.env.DB_PASS || "", {
-    host   : "localhost",
-    dialect: "mysql"
-});
+// Configuração da conexão com fallback para localhost
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE || "projeto_PI", // Nome do banco
+  process.env.MYSQLUSER || "root", // Usuário
+  process.env.MYSQLPASSWORD || "", // Senha
+  {
+    host: process.env.MYSQLHOST || "localhost", // Host
+    port: process.env.MYSQLPORT || 3306, // Porta
+    dialect: "mysql",
+  }
+);
 
-  // Testar a conexão com o banco de dados
-sequelize.authenticate()
-    .then(() => console.log('Conectado ao banco de dados'))
-    .catch(err => console.log('Erro: ' + err));
+// Testar a conexão com o banco de dados
+sequelize
+  .authenticate()
+  .then(() => console.log("Conectado ao banco de dados"))
+  .catch((err) => console.error("Erro ao conectar: ", err));
 
 module.exports = sequelize;
