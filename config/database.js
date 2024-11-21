9f56a483e56835f4ea794cffcea6fef95ba96cae
Arthur Lima <arthurlima121213@gmail.com>
2024-11-21 01:58:35 -0300

    Ajustes para deploy

--- config/database.js
+++ config/database.js
@@ -1,16 +1,22 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME || "projeto_PI", 
-                                process.env.DB_USER || "root", 
-                                process.env.DB_PASS || "", {
-    host   : "localhost",
-    dialect: "mysql"
-});
+// Configuração da conexão com o banco de dados no Railway
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE || "railway", // Nome do banco de dados
+  process.env.MYSQLUSER || "root", // Usuário
+  process.env.MYSQLPASSWORD || "", // Senha
+  {
+    host: process.env.MYSQLHOST || "localhost", // Host (Agora usando a variável do Railway)
+    port: process.env.MYSQLPORT || 3306, // Porta
+    dialect: "mysql", // Dialeto MySQL
+  }
+);
 
-  // Testar a conexão com o banco de dados
-sequelize.authenticate()
-    .then(() => console.log('Conectado ao banco de dados'))
-    .catch(err => console.log('Erro: ' + err));
+// Testar a conexão com o banco de dados
+sequelize
+  .authenticate()
+  .then(() => console.log("Conectado ao banco de dados"))
+  .catch((err) => console.error("Erro ao conectar: ", err));
 
 module.exports = sequelize;
